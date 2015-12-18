bde07347806b63186c963764fc7266968fdafe75
David B Mittelman <dbmittelman@gmail.com>
2015-12-17 17:49:57 -0800

    Tagging - tag multiple images at once

--- index.js
+++ index.js
@@ -18,6 +18,31 @@ var formatSingleResult = function(body) {
   return ret
 }
 
+var formatResults = function(body) {
+  var results = body.results
+  results = _.map(results, function(result) {
+    var tagResults = _.zip(result.result.tag.classes, result.result.tag.concept_ids, result.result.tag.probs)
+    var tags = _.map(tagResults, function(tag) {
+      return {
+        class: tag[0],
+        conceptId: tag[1],
+        probability: tag[2]
+      }
+    })
+    var ret = {
+      docId: result.docid,
+      tags: tags
+    }
+    return ret
+  })
+
+  if (results.length < 2) {
+    results = _.first(results)
+  }
+  return results
+}
+
+
 Clarifai.prototype.getAccessToken = function(cb) {
   _this = this
   needle.post('https://api.clarifai.com/v1/token/', {
@@ -29,6 +54,7 @@ Clarifai.prototype.getAccessToken = function(cb) {
     _this.expiresIn = body.expires_in
     _this.scope = body.scope
     _this.tokenType = body.token_type
+    console.log("Access Token :: " + _this.accessToken)
     cb(err, body)
   });
 }
@@ -49,7 +75,34 @@ Clarifai.prototype.tagImageFromUrl = function(url, cb) {
         _this.tagImageFromUrl(url, cb)
       })
     } else {
-      cb(err, formatSingleResult(body))
+      cb(err, formatResults(body))
+    }
+  });
+}
+
+Clarifai.prototype.tagImagesFromUrls = function(urls, cb) {
+  var data = ""
+
+  if (!_.isArray(urls)) {
+    urls = [urls]
+  }
+
+  for (var url of urls) {
+    data += encodeURI("url=" + url) + "&"
+  }
+
+  var options = {
+    headers: { 'Authorization': this.tokenType + ' ' + this.accessToken },
+  }
+  _this = this
+
+  needle.post('https://api.clarifai.com/v1/tag/', data, options, function(err, resp, body) {
+    if (body.status_code == 'TOKEN_INVALID') {
+      _this.getAccessToken(function(err, resp) {
+        _this.tagImageFromUrl(url, cb)
+      })
+    } else {
+      cb(err, formatResults(body))
     }
   });
 }
--- test/index.js
+++ test/index.js
@@ -15,11 +15,28 @@ describe('#Authentication', function() {
 describe('#Tagging', function() {
   it('should tag an image from a url', function(done) {
     var url = 'http://www.clarifai.com/img/metro-north.jpg'
-    client.tagImageFromUrl(url, function(err, resp) {
+    client.tagImagesFromUrls(url, function(err, resp) {
       should.not.exist(err)
       resp.should.have.property('docId')
       resp.should.have.property('tags').with.length.above(0)
       done()
     })
   });
+
+  it('should tag multiple images from a set of urls', function(done) {
+    var urls = [
+      'http://www.clarifai.com/img/metro-north.jpg',
+      'http://www.clarifai.com/img/metro-north.jpg',
+      'http://www.clarifai.com/img/metro-north.jpg'
+    ]
+
+    client.tagImagesFromUrls(urls, function(err, resp) {
+      should.not.exist(err)
+      resp.should.have.length(3)
+      resp[0].should.have.property('docId')
+      resp[0].should.have.property('tags').with.length.above(0)
+      done()
+    })
+  });
+
 });
