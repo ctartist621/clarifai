b7bd9ae9bfcddf3f367047394db48cc6e09505cd
David B Mittelman <dbmittelman@gmail.com>
2015-12-17 21:56:49 -0800

    Tagging - tag multiple videos

--- test/index.js
+++ test/index.js
@@ -89,6 +89,28 @@ describe('#Tagging', function() {
         done()
       })
     });
+
+    it('should tag multiple videos from a set of urls', function(done) {
+     this.timeout(60000);
+      var urls = [
+        'http://html5videoformatconverter.com/data/images/happyfit2.mp4',
+        'http://html5videoformatconverter.com/data/images/happyfit2.mp4',
+        'http://html5videoformatconverter.com/data/images/happyfit2.mp4',
+      ]
+      client.tagVideosFromUrls(urls, function(err, resp) {
+        should.not.exist(err)
+        resp.should.have.length(3)
+        resp[0].should.have.property('docId')
+        resp[0].should.have.property('docIdStr')
+        resp[0].should.have.property('timestamps').with.length.above(0)
+        resp[0].timestamps[0].should.have.property('timestamp')
+        resp[0].timestamps[0].should.have.property('tags').with.length.above(0)
+        resp[0].timestamps[0].tags[0].should.have.property('class')
+        resp[0].timestamps[0].tags[0].should.have.property('conceptId')
+        resp[0].timestamps[0].tags[0].should.have.property('probability')
+        done()
+      })
+    });
   });
 });
 
